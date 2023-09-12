ed749aa2a9f9e9b54dcbb2cb70dcf13a5f3d80e3
Alvaro Cea <alvaro_cea@outlook.com>
2023-09-12 15:35:40 +0100

    finanlised aero DLM model for Sailplane

--- Generators/Models/SailPlane/aero_inp.py
+++ Generators/Models/SailPlane/aero_inp.py
@@ -9,9 +9,9 @@ def wb2w(point_le, x_te, z_bottom, pct2le, pct2te):
     LE = point_le[0] - chord * pct2le
     point_LE = [LE, point_le[1], (point_le[2] +
                                   z_bottom) / 2]
-    return point_LE, x
+    return point_LE, chord
 
-NumSurfaces = 7
+NumSurfaces = 9
 aero = {}
 caero1 = [{} for i in range(NumSurfaces)]
 paero1 = [{} for i in range(NumSurfaces)]
@@ -35,18 +35,20 @@ x43=[]
 nspan=[]
 nchord=[]
 set1x=[]
-
+components = []
 ##############FUSELAGE#######################
 ###
 ##############InnerWingR#######################
-point_LE1, l1 = wb2w(point_le=[3.94, 0, 0.231],
+components.append("InnerWingR")
+zcorr = 0.40135 / 0.3795
+point_LE1, l1 = wb2w(point_le=[3.94, 0, zcorr * 0.231],
                      x_te=8.49,
-                     z_bottom=-0.49,
+                     z_bottom=-0.49 * zcorr,
                      pct2le=0.15,
                      pct2te=0.6)
-point_LE2, l2 = wb2w(point_le=[3.94, 2.793, 0.231],
+point_LE2, l2 = wb2w(point_le=[3.94, 2.793, 0.231 * zcorr],
                      x_te=8.49,
-                     z_bottom=-0.49,
+                     z_bottom=-0.49 * zcorr,
                      pct2le=0.15,
                      pct2te=0.6)
 
@@ -54,26 +56,27 @@ p1.append(point_LE1)
 x12.append(l1)
 p4.append(point_LE2)
 x43.append(l2)
-nspan.append(5)
+nspan.append(4)
 nchord.append(10)
 #set1x.append([10040069, 10040066, 10040063, 10040060, 10040000])
 set1x.append([999023, 999022, 999021, 999020, 999000])
 ##################OuterWingR#####################
-point_LE1, l1 = wb2w(point_le=[3.94, 2.793, 0.231],
+components.append("OuterWingR")
+point_LE1, l1 = wb2w(point_le=[3.94, 2.793, 0.231 * zcorr],
                      x_te=8.49,
-                     z_bottom=-0.49,
+                     z_bottom=-0.49 * zcorr,
                      pct2le=0.15,
                      pct2te=0.6)
-point_LE2, l2 = wb2w(point_le=[19.23, 28.8, -0.304],
+point_LE2, l2 = wb2w(point_le=[19.23, 28.8, -0.304 * zcorr],
                      x_te=20.47,
-                     z_bottom=-0.455,
+                     z_bottom=-0.455 * zcorr,
                      pct2le=0.15,
                      pct2te=0.6)
 p1.append(point_LE1)
 x12.append(l1)
 p4.append(point_LE2)
 x43.append(l2)
-nspan.append(5)
+nspan.append(19)
 nchord.append(10)
 set1x.append([999000,
               999001,
@@ -97,14 +100,15 @@ set1x.append([999000,
               999019])
 
 ##############InnerWingL#######################
-point_LE1, l1 = wb2w(point_le=[3.94, 0, 0.231],
+components.append("InnerWingL")
+point_LE1, l1 = wb2w(point_le=[3.94, 0, 0.231 * zcorr],
                      x_te=8.49,
-                     z_bottom=-0.49,
+                     z_bottom=-0.49 * zcorr,
                      pct2le=0.15,
                      pct2te=0.6)
-point_LE2, l2 = wb2w(point_le=[3.94, -2.793, 0.231],
+point_LE2, l2 = wb2w(point_le=[3.94, -2.793, 0.231 * zcorr],
                      x_te=8.49,
-                     z_bottom=-0.49,
+                     z_bottom=-0.49 * zcorr,
                      pct2le=0.15,
                      pct2te=0.6)
 
@@ -112,17 +116,18 @@ p1.append(point_LE1)
 x12.append(l1)
 p4.append(point_LE2)
 x43.append(l2)
-nspan.append(5)
+nspan.append(4)
 nchord.append(10)
 #set1x.append([10040069, 10040066, 10040063, 10040060, 10040000])
 set1x.append([999023, 999057, 999056, 999055, 999035])
 ##################OuterWingL#####################
-point_LE1, l1 = wb2w(point_le=[3.94, -2.793, 0.231],
+components.append("OuterWingL")
+point_LE1, l1 = wb2w(point_le=[3.94, -2.793, 0.231 * zcorr],
                      x_te=8.49,
                      z_bottom=-0.49,
                      pct2le=0.15,
                      pct2te=0.6)
-point_LE2, l2 = wb2w(point_le=[19.23, -28.8, -0.304],
+point_LE2, l2 = wb2w(point_le=[19.23, -28.8, -0.304 * zcorr],
                      x_te=20.47,
                      z_bottom=-0.455,
                      pct2le=0.15,
@@ -131,7 +136,7 @@ p1.append(point_LE1)
 x12.append(l1)
 p4.append(point_LE2)
 x43.append(l2)
-nspan.append(5)
+nspan.append(19)
 nchord.append(10)
 set1x.append([999035,
               999036,
@@ -158,6 +163,7 @@ set1x.append([999035,
 ###
 #####################TAILPLANE######
 #hstabilizerInnerR
+components.append("hstabilizerInnerR")
 point_LE1, l1 = wb2w(point_le=[36.363, 0, 3.225],
                      x_te=40.236,
                      z_bottom=2.875,
@@ -173,10 +179,11 @@ p1.append(point_LE1)
 x12.append(l1)
 p4.append(point_LE2)
 x43.append(l2)
-nspan.append(2)
+nspan.append(1)
 nchord.append(10)
 set1x.append([999034, 999024])
 #hstabilizerOuterR
+components.append("hstabilizerOuterR")
 point_LE1, l1 = wb2w(point_le=[36.363, 0.5, 3.225],
                      x_te=40.236,
                      z_bottom=2.875,
@@ -192,7 +199,7 @@ p1.append(point_LE1)
 x12.append(l1)
 p4.append(point_LE2)
 x43.append(l2)
-nspan.append(10)
+nspan.append(9)
 nchord.append(10)
 set1x.append([999024,
               999025,
@@ -205,6 +212,7 @@ set1x.append([999024,
               999032,
               999033])
 #hstabilizerInnerL
+components.append("hstabilizerInnerL")
 point_LE1, l1 = wb2w(point_le=[36.363, 0, 3.225],
                      x_te=40.236,
                      z_bottom=2.875,
@@ -220,12 +228,13 @@ p1.append(point_LE1)
 x12.append(l1)
 p4.append(point_LE2)
 x43.append(l2)
-nspan.append(2)
+nspan.append(1)
 nchord.append(10)
 set1x.append([999034,
               999068])
 
 #hstabilizerOuterL
+components.append("hstabilizerOuterL")
 point_LE1, l1 = wb2w(point_le=[36.363, -0.5, 3.225],
                      x_te=40.236,
                      z_bottom=2.875,
@@ -241,7 +250,37 @@ p1.append(point_LE1)
 x12.append(l1)
 p4.append(point_LE2)
 x43.append(l2)
-nspan.append(10)
+nspan.append(9)
+nchord.append(10)
+set1x.append([999058,
+              999059,
+              999060,
+              999061,
+              999062,
+              999063,
+              999064,
+              999065,
+              999066,
+              999067])
+#vstabilizer
+# WARNING: y-midplane not implemented
+components.append("vstabilizer")
+point_LE1, l1 = wb2w(point_le=[34.54, 0., 3.3],
+                     x_te=39.983,
+                     z_bottom=3.3,
+                     pct2le=0.15,
+                     pct2te=0.6)
+point_LE2, l2 = wb2w(point_le=[42.236, 0., 13],
+                     x_te=44.227,
+                     z_bottom=13,
+                     pct2le=0.15,
+                     pct2te=0.6)
+
+p1.append(point_LE1)
+x12.append(l1)
+p4.append(point_LE2)
+x43.append(l2)
+nspan.append(9)
 nchord.append(10)
 set1x.append([999068,
               999069,
@@ -253,7 +292,8 @@ set1x.append([999068,
               999075,
               999076,
               999077])
-          
+
+
 def dlm(NumSurfaces,paero1,caero1,aelist,set1,spline67,p1,x12,p4,x43,nspan,nchord,set1x):
     npanels = 0
     for i in range(NumSurfaces):
@@ -269,15 +309,41 @@ def dlm(NumSurfaces,paero1,caero1,aelist,set1,spline67,p1,x12,p4,x43,nspan,nchor
         caero1[i]['nspan'] = nspan[i]#4  # boxes across y-direction
         caero1[i]['nchord'] = nchord[i]#10  # boxes across x-direction
         aelist[i]['sid'] = 666000 +i
-        aelist[i]['elements'] = range(caero1[i]['eid'],caero1[i]['eid']+
-                                      caero1[i]['nspan']*caero1[i]['nchord'])
+        aelist[i]['elements'] = list(range(caero1[i]['eid'],caero1[i]['eid']+
+                                      caero1[i]['nspan']*caero1[i]['nchord']))
         set1[i]['sid'] = aelist[i]['sid']
         set1[i]['ids'] = set1x[i]
         sp_num = 6
-        spline67[i]=[sp_num,'SPLINE%s'%sp_num,aelist[i]['sid'],caero1[i]['eid'],aelist[i]['sid'],None,
+        spline67[i]=['SPLINE%s'%sp_num,aelist[i]['sid'],caero1[i]['eid'],aelist[i]['sid'],None,
                          set1[i]['sid']]  # EID,CAERO,AELIST,NONE,SETG
         npanels +=  nspan[i]*nchord[i]
     return npanels
 
     
 npanels = dlm(NumSurfaces,paero1,caero1,aelist,set1,spline67,p1,x12,p4,x43,nspan,nchord,set1x)
+
+import PostProcessing.panels as panels
+from pyNastran.bdf.bdf import BDF
+################################################################################
+# AERODYNAMICS
+################################################################################
+machs = [0.8]
+reduced_freqs = np.linspace(1e-9,1, 20)
+
+model=BDF(debug=True,log=None)
+for i in range(NumSurfaces):
+    model.add_aelist(**aelist[i], comment=components[i])
+    model.add_set1(**set1[i], comment=components[i])
+    model.add_caero1(**caero1[i], comment=components[i])
+    model.add_paero1(**paero1[i], comment=components[i])
+    model.add_card(spline67[i], 'SPLINE6', comment=components[i])
+model.add_mkaero1(machs,reduced_freqs)
+model.write_bdf("./aero.bdf")
+
+
+# from pyNastran.op2.export_to_vtk import export_to_vtk_filename
+# export_to_vtk_filename(bdf_filename, op2_filename, vtk_filename,
+#                             debug=False, log=None)
+
+grid = panels.caero2grid(components, caero1)
+panels.build_gridmesh(grid, 'dlm_mesh')
--- PostProcessing/panels.py
+++ PostProcessing/panels.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pyvista
+import pathlib
 
 def line_points(n: int, p1: np.ndarray, p2:np.ndarray) -> np.ndarray:
     """Array with n+1 points from p1 to p2
@@ -51,7 +54,7 @@ def line_points_y(n: int, p1: np.ndarray, p2:np.ndarray) -> np.ndarray:
     if np.allclose(p1, p2):
         raise ValueError("p1 equals p2")
     pn = (p2-p1) / (p2[1] - p1[1])
-    step = p2[1]-p1[1] / n
+    step = (p2[1]-p1[1]) / n
     px=[]
     for i in range(n):
       px.append(p1+step*i*pn) 
@@ -63,13 +66,31 @@ def caero2corners(le1, le2, l1, l2):
 
     te1 = [le1[0] + l1, le1[1], le1[2]]
     te2 = [le2[0] + l2, le2[1], le2[2]]
-    return np.hstack([le1, le2, te1, te2])
+    return np.vstack([le1, le2, te1, te2])
 
 def lt_points(n_span, le1, le2, te1, te2):
 
-    leading_edge = line_points_y(n_span, le1, le2)
-    trailiing_edge = line_points_y(n_span, te1, te2)
-    return leading_edge, trailiing_edge
+    leading_edge = line_points(n_span, le1, le2)
+    trailing_edge = line_points(n_span, te1, te2)
+    return leading_edge, trailing_edge
+
+def caero2grid(components, caeros):
+
+    component_grid = {k: dict() for k in components}
+    for i, k in enumerate(components):
+        Xcorners = caero2corners(caeros[i]['p1'],
+                                 caeros[i]['p4'],
+                                 caeros[i]['x12'],
+                                 caeros[i]['x43'])
+        leading_edge, trailing_edge = lt_points(caeros[i]['nspan'],
+                                                Xcorners[0],
+                                                Xcorners[1],
+                                                Xcorners[2],
+                                                Xcorners[3])
+        component_grid[k]['leading_edge'] = leading_edge
+        component_grid[k]['trailing_edge'] = trailing_edge
+        component_grid[k]['chordwise_points'] = caeros[i]['nchord']
+    return component_grid
 
 def build_pyvista(le, te, chordwise_points=10) -> tuple[np.ndarray, np.ndarray]:
 
@@ -89,8 +110,8 @@ def build_pyvista(le, te, chordwise_points=10) -> tuple[np.ndarray, np.ndarray]:
     points = np.vstack([points, line_])        
     return points, np.array(cells)
 
-def build_gridmesh(nchord, save_file=None,
-                   save_dir=None, build_symmetric=True):
+def build_gridmesh(components, save_file=None,
+                   save_dir=None):
 
     for k, v in components.items():
     
@@ -102,7 +123,7 @@ def build_gridmesh(nchord, save_file=None,
             if save_dir is not None:
                 file_path = pathlib.Path(save_dir)
             else:
-                file_path = pathlib.Path().cwd() / "modes_paraview"
-            self.file_path.mkdir(parents=True, exist_ok=True)
-            mesh.save(self.file_path / f"{save_file}_{k}.ply",
+                file_path = pathlib.Path().cwd() / "paraview"
+            file_path.mkdir(parents=True, exist_ok=True)
+            mesh.save(file_path / f"{save_file}_{k}.ply",
                       binary=False)
