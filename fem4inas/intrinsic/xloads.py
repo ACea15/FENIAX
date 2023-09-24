86186ab6d0ed3eb105d7624c13060948b9a9c3d4
Alvaro Cea <alvaro_cea@outlook.com>
2023-09-24 20:59:49 +0100

    make xloads interpolation jit-compatible

--- fem4inas/intrinsic/xloads.py
+++ fem4inas/intrinsic/xloads.py
@@ -1,10 +1,38 @@
 import jax.numpy as jnp
 import jax
+from functools import partial
+
+# @partial(jax.jit, static_argnames=["t", "x"])
+# def linear_interpolation(t, x, force_tensor):
+
+#     xindex_upper = jnp.where(x >= t)[0][0]
+#     xindex_lower = jnp.where(x <= t)[0][-1]
+#     x_upper = x[xindex_upper]
+#     x_lower = x[xindex_lower]
+#     weight_upper = jax.lax.select(xindex_upper == xindex_lower,
+#                                   0.5,
+#                                   (t - x_lower) / (x_upper - x_lower))
+#     weight_lower = jax.lax.select(xindex_upper == xindex_lower,
+#                                   0.5,
+#                                   (x_upper - t) / (x_upper - x_lower))
+
+#     f_upper = force_tensor[xindex_upper]
+#     f_lower = force_tensor[xindex_lower]
+#     f_interpol = weight_upper * f_upper + weight_lower  * f_lower
+#     return f_interpol
 
 def linear_interpolation(t, x, force_tensor):
 
-    xindex_upper = jnp.where(x >= t)[0][0]
-    xindex_lower = jnp.where(x <= t)[0][-1]
+    len_x = x.shape[0]
+    xindex_upper = jnp.argwhere(jax.lax.select(x >= t,
+                                               jnp.ones(len_x),
+                                               jnp.zeros(len_x)),
+                                size=1)[0][0]#jnp.where(x >= t)[0][0]
+    index_equal = jnp.sum(jax.lax.select(x == t,
+                                             jnp.ones(len_x),
+                                             jnp.zeros(len_x)),
+                          dtype=int)
+    xindex_lower = xindex_upper - 1 + index_equal #jnp.where(x <= t)[0][-1]
     x_upper = x[xindex_upper]
     x_lower = x[xindex_lower]
     weight_upper = jax.lax.select(xindex_upper == xindex_lower,
