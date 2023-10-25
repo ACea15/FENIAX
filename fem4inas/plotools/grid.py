80b73e59b5031f02d91642eaf3f206f9d197596f
Alvaro Cea <alvaro_cea@outlook.com>
2023-10-25 16:59:14 +0100

    add merging to grid in plotools

--- fem4inas/plotools/grid.py
+++ fem4inas/plotools/grid.py
@@ -6,11 +6,16 @@ from pyNastran.bdf.bdf import BDF
 
 
 def transform_grid_rigid(ra, Rab, pa):
-    f = jax.vmap(lambda ra_i, Rab_i, pa_i: ra_i + Rab_i @ pa_i,
+    f = jax.vmap(lambda ra_i, Rab_i, pa_i: ra_i.reshape((3, 1)) + Rab_i @ pa_i,
                  in_axes=(1, 2, 2), out_axes=2)
     pa_deform = f(ra, Rab, pa)
     return pa_deform
 
+def transform_arm_rigid(Rab, pa):
+    f = jax.vmap(lambda Rab_i, pa_i: Rab_i @ pa_i,
+                 in_axes=(2, 2), out_axes=2)
+    pa_deform = f(Rab, pa)
+    return pa_deform
 
 @dataclass
 class AeroGrid:
@@ -52,14 +57,17 @@ class Component:
     link_m0m1: dict
     link_m0mx: dict
     data_m0: dict
+    data_m1: dict
     pa_m1: jnp.array
     pa_m1_tensor: jnp.array
     ra: jnp.array = None
     Rab: jnp.array = None
+    data_mx: jnp.array = None
+    data_mx_tensor: jnp.array = None
     pa_mx: jnp.array = None
     pa_mx_tensor: jnp.array = None
 
-    def __pos_init__(self):
+    def __post_init__(self):
         self.len_pa = len(self.pa_m1)
         self.link_m0 = list(self.link_m0mx.keys())
         self.link_mx = list(self.link_m0mx.values())
@@ -96,23 +104,30 @@ class ASETModel(Model):
         self.components = {ki: None for ki in self.aerogrid.labels.keys()}
         self.component_names = list(self.components.keys())
         self.link_models()
+        self.merge_components()
+
     def link_models(self):
 
         for i, ki in enumerate(self.component_names):
             print(f"setting component {ki}")
             m0_ids = self.model0_ids[i]
+            data_m1 = self.aerogrid.get('points', ki)
             link_m0m1, data_m0, pa_m1 = self.point2aset(self.model,
-                                                        self.aerogrid.get('points', ki),
+                                                        data_m1,
                                                         m0_ids)
             link_m0mx = self.link_solution(m0_ids, data_m0)
             pa_m1_tensor = self.build_m1_tensor(pa_m1, link_m0m1)
             self.components[ki] = Component(link_m0m1, link_m0mx,
-                                            data_m0, pa_m1, pa_m1_tensor)
+                                            data_m0, data_m1, pa_m1, pa_m1_tensor)
+
     def link_solution(self, asets, data_m0):
-        link_m0mx = self.aset2id_intrinsic(self.modelx_data, asets, data_m0, self.tol_identification)
+        link_m0mx = self.aset2id_intrinsic(self.modelx_data, asets, data_m0,
+                                           self.tol_identification)
         return link_m0mx
 
     def set_solution(self, ra, Rab):
+        """
+        """
         self.ra = ra
         self.Rab = Rab
         for i, ki in enumerate(self.component_names):
@@ -122,7 +137,9 @@ class ASETModel(Model):
             self.components[ki].ra = ra_ci
             self.components[ki].Rab = Rab_ci
             self.map_mxm1(ki)
-            
+        self.merge_data("data_mx")
+        self.merge_data("pa_mx")
+        
     def build_m1_tensor(self, pa_m1, link_m0m1):
 
         pa_points = list(link_m0m1.values())
@@ -136,40 +153,94 @@ class ASETModel(Model):
         return pa_tensor
 
     def tensor2array(self, ki, pa_tensor):
-        pa_points = self.components[ki].link_mx
-        pa_array = np.zeros(3, self.components[ki].len_pa)
+        pa_points = self.components[ki].link_m1
+        pa_array = np.zeros((3, self.components[ki].len_pa))
+        #import pdb; pdb.set_trace()
         for i, arm_i in enumerate(pa_points):
-            pa_array[arm_i] = pa_tensor[:,:, i]
+            pa_array[:, arm_i] = pa_tensor[:,:, i]
         return pa_array
 
     def map_mxm1(self, ki):
 
-        self.components[ki].pa_mx_tensor = transform_grid_rigid(self.components[ki].ra,
-                                                                self.components[ki].Rab,
-                                                                self.components[ki].pa_m1_tensor)
-        self.components[ki].pa_mx = self.tensor2array(ki, self.components[ki].pa_mx_tensor)
+        self.components[ki].data_mx_tensor = transform_grid_rigid(self.components[ki].ra,
+                                                                  self.components[ki].Rab,
+                                                                  self.components[ki].pa_m1_tensor)
+        self.components[ki].data_mx = self.tensor2array(ki,
+                                                        self.components[ki].data_mx_tensor)
+        self.components[ki].pa_mx_tensor = transform_arm_rigid(self.components[ki].Rab,
+                                                               self.components[ki].pa_m1_tensor)
+        self.components[ki].pa_mx = self.tensor2array(ki,
+                                                      self.components[ki].pa_mx_tensor)
 
     def merge_components(self):
 
-        index_merged = {}
+        self.index_merged = {}
         for i, ki in enumerate(self.component_names):
+            index_pa_merged = np.array([])
             if i == 0:
-                pa_merged = self.components[ki].pa
+                self.pa_merged = self.components[ki].pa_m1
+                self.pa = self.components[ki].pa_m1
+                self.datam1_merged = self.components[ki].data_m1
+                self.datam1 = self.components[ki].data_m1
+
             else:
-                index_pa_merged = np.array([])
                 index = []
-                for i, pai in enumerate(self.components[ki].pa):
-                    ind = np.where(np.linalg.norm(pai - pa_merged) <
+                for i, datam1_i in enumerate(self.components[ki].data_m1):
+                    ind = np.where(np.linalg.norm(datam1_i - self.datam1_merged, axis=1) <
                                    self.tol_identification)[0]
                     if len(ind) > 0:
-                        index_pa_merged = np.hstack([index_pa_merged, ])
-                index_merged[ki] = [len(pa_merged), index]
-                pa_merged = np.vstack([pa_merged, self.components[ki].pa])
+                        #import pdb; pdb.set_trace()
+                        #print(ind)
+                        index_pa_merged = np.hstack([index_pa_merged, ind])
+                        index.append(i)
+                index = np.array(index)
+                self.index_merged[ki] = [len(self.datam1_merged), index, index_pa_merged]
+                self.datam1_merged = np.vstack([self.datam1_merged,
+                                                self.components[ki].data_m1])
+                self.pa_merged = np.vstack([self.pa_merged, self.components[ki].pa_m1])
+                self.pa = np.vstack([self.pa_merged, self.components[ki].pa_m1])
+                self.datam1 = np.vstack([self.datam1, self.components[ki].data_m1])
                 if len(index) > 0:
-                    pa_merged = np.delete(pa_merged, index_merged[ki][0] + index, axis=0)
-        
+                    self.pa_merged = np.delete(self.pa_merged,
+                                          self.index_merged[ki][0] + index, axis=0)
+                    self.datam1_merged = np.delete(self.datam1_merged,
+                                                   self.index_merged[ki][0] + index, axis=0)
+    def merge_data(self, data_name:str):
+
+        self.index_merged = {}
+        for i, ki in enumerate(self.component_names):
+            if i == 0:
+                data = getattr(self.components[ki], data_name)
+                data_merged = getattr(self.components[ki], data_name)
+
+            else:
+                # index = []
+                # for i, data_i in enumerate(getattr(self.components[ki], data_name)):
+                #     ind = np.where(np.linalg.norm(data_i - self.datam1_merged, axis=1) <
+                #                    self.tol_identification)[0]
+                #     if len(ind) > 0:
+                #         #import pdb; pdb.set_trace()
+                #         #print(ind)
+                #         index_pa_merged = np.hstack([index_pa_merged, ind])
+                #         index.append(i)
+                # index = np.array(index)
+                # self.index_merged[ki] = [len(self.datam1_merged), index, index_pa_merged]
+                data_ki = getattr(self.components[ki], data_name)
+                #import pdb; pdb.set_trace()
+                data = np.hstack([data_merged,
+                                  data_ki])
+                data_merged = np.hstack([data_merged,
+                                         data_ki])
+                index = self.index_merged[ki][1]
+                if len(index) > 0:
+                    data_merged = np.delete(data_merged,
+                                            self.index_merged[ki][0] + index,
+                                            axis=0)
+        setattr(self, f"{data_name}", data)
+        setattr(self, f"{data_name}_merged", data_merged)
+    
     @staticmethod
-    def point2aset2(model, points, asets) -> tuple[np.array]:
+    def point2aset2(model, points, asets) -> tuple[dict, np.ndarray, np.ndarray]:
         """
         Links arbitrary points to the asets (beam) nodes
         """
