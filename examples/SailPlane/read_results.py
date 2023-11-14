0bc6fa85a3b40aa99dc429059fe4a803580b2c53
Alvaro Cea <alvaro_cea@outlook.com>
2023-11-14 19:27:03 +0000

    add read_results sailplane

---
+++ examples/SailPlane/read_results.py
@@ -0,0 +1,17 @@
+from pyNastran.op2.op2 import OP2
+import pandas as pd
+import plotly.express as px
+
+op2 = OP2()
+op2.set_additional_matrices_to_read({b'OPHP':False})
+op2.read_op2("./NASTRAN/SOL146/run_cao.op2")
+
+
+
+data = op2.displacements[1].data
+df = pd.DataFrame(dict(#time=op2.displacements[1].dts,
+                  z=data[:,19,2]))
+
+fig = px.line(df,  title='Life expectancy in Canada')
+fig.show()
+
