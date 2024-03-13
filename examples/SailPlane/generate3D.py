d4c916ac0844632735f1f4caef3979a493b0611d
Alvaro Cea <alvaro_cea@outlook.com>
2024-03-13 20:33:48 +0000

    add generate 3D sailplane

--- examples/SailPlane/generate3D.py
+++ examples/SailPlane/generate3D.py
@@ -4,6 +4,7 @@ from pyNastran.op2.op2 import OP2
 import pandas as pd
 import importlib
 import numpy as np
+import jax.numpy as jnp
 from fem4inas.preprocessor import solution
 import fem4inas.plotools.interpolation as interpolation
 import fem4inas.plotools.nastranvtk.bdfdef as bdfdef
@@ -35,8 +36,8 @@ uintrinsic = np.zeros((6, len(nodesX), 3))
 rintrinsic = np.zeros((6, len(nodesX), 3))
 
 for i, ti in enumerate(time):
-    ra = soli.data.dynamicsystem_s1.ra[ti]
-    Rab = soli.data.dynamicsystem_s1.Cab[ti]
+    ra = soli.data.staticsystem_s1.ra[ti]
+    Rab = soli.data.staticsystem_s1.Cab[ti]
     gridmodel.set_solution(ra, Rab, R0ab)
     disp, coord = interpolation.compute(gridmodel.model1_coord,
                                         gridmodel.model1x_coord,
@@ -50,24 +51,36 @@ for i, ti in enumerate(time):
             bdf_def.plot_vtk(f"./generateParaview_static/sp_L{i}.bdf")
 
 
-op2_model = OP2()
-op2_model.set_additional_matrices_to_read({b'OPHP':False, b'OUG1':False, b'OUGV1': False})
-
-op2_model.read_op2("/Users/ac5015/pCloud Drive/Imperial/Computations/FEM4INAS/Models/SailPlane/SP400/SailPlane_MakeMatc.op2", skip_undefined_matrices=True, combine=False)
-#op2_model.read_op2("./NASTRAN/wing400d.op2", skip_undefined_matrices=True)
-
-op2model = op2reader.NastranReader("/Users/ac5015/pCloud Drive/Imperial/Computations/FEM4INAS/Models/SailPlane/SP400/SailPlane_MakeMatc.op2",
-                                   "/Users/ac5015/pCloud Drive/Imperial/Computations/FEM4INAS/Models/SailPlane/SP400/SailPlane_MakeMatc.bdf",
+op2model = op2reader.NastranReader("./NASTRAN/static400/run_all.op2",
+                                   "./NASTRAN/static400/run_all.bdf",
                                    static=True)
 op2model.readModel()
 tnastran, unastran = op2model.displacements()
 
 
+bdf_def2 = bdfdef.DefBdf("./NASTRAN/static400/run_all.bdf")
+nodesX2 = bdf_def2.get_nodes()
+
+for i, ti in enumerate(time):
+    if i % time_plot == 0:
+        tstep = int(i / time_plot)
+        coord = nodesX2 + unastran[i,:,:3]
+        bdf_def2.update_bdf(coord, bdf_def.sorted_nodeids)
+        bdf_def2.plot_vtk(f"./generateParaview_static/sp_op2L{i}.bdf")
 
 
+err = jnp.array([jnp.linalg.norm(unastran[i,:,:3] - uintrinsic[i]) / 3313 for i in range(6)])
+jnp.save("./sp_err.npy",err)
 
-op2model = op2reader.NastranReader("./NASTRAN/static400/run.op2",
-                                   "./NASTRAN/static400/run.bdf",
-                                   static=True)
-op2model.readModel()
-tnastran, unastran = op2model.displacements()
+
+# op2_model = OP2()
+# op2_model.set_additional_matrices_to_read({b'OPHP':False, b'OUG1':False, b'OUGV1': False})
+
+# op2_model.read_op2("/Users/ac5015/pCloud Drive/Imperial/Computations/FEM4INAS/Models/SailPlane/SP400/SailPlane_MakeMatc.op2", skip_undefined_matrices=True, combine=False)
+# #op2_model.read_op2("./NASTRAN/wing400d.op2", skip_undefined_matrices=True)
+
+# op2model = op2reader.NastranReader("/Users/ac5015/pCloud Drive/Imperial/Computations/FEM4INAS/Models/SailPlane/SP400/SailPlane_MakeMatc.op2",
+#                                    "/Users/ac5015/pCloud Drive/Imperial/Computations/FEM4INAS/Models/SailPlane/SP400/SailPlane_MakeMatc.bdf",
+#                                    static=True)
+# op2model.readModel()
+# tnastran, unastran = op2model.displacements()
--- examples/wingSP/generate3D.py
+++ examples/wingSP/generate3D.py
@@ -3,6 +3,7 @@ from pyNastran.bdf.bdf import BDF
 import pandas as pd
 import importlib
 import numpy as np
+import jax.numpy as jnp
 from fem4inas.preprocessor import solution
 import fem4inas.unastran.op2reader as op2reader
 import fem4inas.plotools.interpolation as interpolation
@@ -18,21 +19,21 @@ X = df_grid[['x1','x2','x3']].to_numpy()
 gridmodel = grid.RBE3Model(bdf_model, X, 1e-3)
 
 
-soli = solution.IntrinsicReader("./results_dynamics")
+soli = solution.IntrinsicReader("./results_dynamics_m50")
 R0ab = soli.data.modes.C0ab
 
 bdf_def = bdfdef.DefBdf("./NASTRAN/wing400d.bdf")
 bdf_def.plot_vtk("./generateParaview_dynamics/spref.bdf")
 
 nodesX = bdf_def.get_nodes()
-time_plot = 1
+time_plot = None 
 time = np.linspace(0, 15000, 10001, dtype=int)
 uintrinsic = np.zeros((10001, len(nodesX), 3))
 rintrinsic = np.zeros((10001, len(nodesX), 3))
 
 for i, ti in enumerate(time):
-    ra = soli.data.dynamicsystem_s1.ra[ti]
-    Rab = soli.data.dynamicsystem_s1.Cab[ti]
+    ra = soli.data.dynamicsystem_s1.ra[i]
+    Rab = soli.data.dynamicsystem_s1.Cab[i]
     gridmodel.set_solution(ra, Rab, R0ab)
     disp, coord = interpolation.compute(gridmodel.model1_coord,
                                         gridmodel.model1x_coord,
@@ -50,3 +51,33 @@ op2model = op2reader.NastranReader("./NASTRAN/wing400d_full.op2",
 op2model.readModel()
 tnastran, unastran = op2model.displacements()
 
+
+
+bdf_def2 = bdfdef.DefBdf("./NASTRAN/wing400d_full.bdf")
+nodesX2 = bdf_def2.get_nodes()
+
+for i, ti in enumerate(time):
+    if time_plot is not None:
+        if i % time_plot == 0:
+            tstep = int(i / time_plot)
+            coord = nodesX2 + unastran[0,i,:,:3]
+            bdf_def2.update_bdf(coord, bdf_def.sorted_nodeids)
+            bdf_def2.plot_vtk(f"./generateParaview_dynamics/spLop2_{tstep}.bdf")
+
+
+op2modelr = op2reader.NastranReader("./NASTRAN/wing400d.op2",
+                                   "./NASTRAN/wing400d.bdf")
+op2modelr.readModel()
+trnastran, urnastran = op2modelr.displacements()
+
+
+err_array = jnp.array([jnp.linalg.norm(unastran[0,i,:,:3] - uintrinsic[i]) / 1255 for i in range(10001)])
+jnp.save("./wsp_err.npy",err_array)
+
+err0 = jnp.max(err_array)
+err = jnp.sum(err_array)/10001
+
+
+import fem4inas.plotools.uplotly as uplotly
+fig = uplotly.lines2d(np.linspace(0., 15., 10001), err_array)
+fig.show()
