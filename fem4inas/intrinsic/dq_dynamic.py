2231533ea6a79b8139b92b1fe3f4267f40cea170
Alvaro Cea <alvaro_cea@outlook.com>
2024-04-30 09:23:55 +0100

    fix passing arguments to computation of Rab0 (not fully satisfied yet)

--- fem4inas/intrinsic/dq_dynamic.py
+++ fem4inas/intrinsic/dq_dynamic.py
@@ -105,10 +105,10 @@ def dq_20G2(t, q, *args):
     qr = q[states['qr']]
     
     Rab = common.computeRab_node0(psi2l, q2, qr, X_xdelta, C0ab,
-                                  (component_names,
+                                  component_names,
                                    num_nodes,
                                    component_nodes,
-                                   component_father))
+                                   component_father)
 
     # no interpolation of gravity in dynamic case
     eta = xloads.eta_pointdead_const(phi1l,
@@ -136,10 +136,10 @@ def dq_20g242(t, q, *args):
     qr = q[states['qr']]
 
     Rab = common.computeRab_node0(psi2l, q2, qr, X_xdelta, C0ab,
-                                  (component_names,
+                                  component_names,
                                   num_nodes,
                                   component_nodes,
-                                   component_father))
+                                  component_father)
     eta = xloads.eta_pointdead(t,
                                phi1l,
                                x,
