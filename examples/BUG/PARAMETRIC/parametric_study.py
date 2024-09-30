c4cd3bc67d417658f8deaa957362c234d2c5e2c1
eisuke <nakagawa-eisuke185@g.ecc.u-tokyo.ac.jp>
2024-09-30 18:17:29 +0900

    add doe option to decode_params

--- examples/BUG/PARAMETRIC/parametric_study.py
+++ examples/BUG/PARAMETRIC/parametric_study.py
@@ -1,7 +1,14 @@
 import itertools
+from scipy.stats import qmc
 
-def decode_params(params)->list[dict]:
+def decode_params(params,doe=False,nsample=10)->list[dict]:
   """
+  If doe is False, it will return a full grid of the parameters
+  If doe is True, it will return a Latin Hypercube of the parameters,
+  in which only the first entries of each list will be used as the lower bound
+  and the last as the upper bound.
+
+
   params['key']=[[val1_1,val1_2,...],[val2_1,val2_2,...],...]
 
   temp=[[[val1_1,val2_1,...],[val1_2,val2_1,...],...],
@@ -13,13 +20,36 @@ def decode_params(params)->list[dict]:
         ...]
   """
   out=[]
-  temp=[]
-  for key in params:
-    vals=params[key]
-    temp.append(list(itertools.product(*vals)))
-  out_vals=list(itertools.product(*temp))
-  for vals in out_vals:
-    out.append({key:list(val) for key,val in zip(params.keys(),vals)})
+  if not doe: # full grid
+    temp=[]
+    for key in params:
+      vals=params[key]
+      temp.append(list(itertools.product(*vals)))
+    out_vals=list(itertools.product(*temp))
+    for vals in out_vals:
+      out.append({key:list(val) for key,val in zip(params.keys(),vals)})
+  elif doe: # design of experiments (Latin Hypercube)
+    ndim=0
+    lowers=[]
+    uppers=[]
+    sids=[]
+    eids=[]
+    sid=0
+    for key in params:
+      vals=params[key]
+      ndim+=len(vals)
+      eid=sid+len(vals)
+      sids.append(sid)
+      eids.append(eid)
+      sid=eid
+      for val in vals:
+        lowers.append(val[0])
+        uppers.append(val[-1])
+    sampler=qmc.LatinHypercube(d=ndim,optimization="random-cd")
+    sample=sampler.random(n=nsample)
+    sample_scaled=qmc.scale(sample,lowers,uppers)
+    for i in range(nsample):
+      out.append({key:list(sample_scaled[i][sid:eid]) for key,sid,eid in zip(params.keys(),sids,eids)})
   return out
 
 
