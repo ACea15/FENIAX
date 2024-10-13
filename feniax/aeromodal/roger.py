00667095d0f6c140dfe83ee3e391a63b04d1c7de
Alvaro Cea <alvaro_cea@outlook.com>
2024-10-13 17:37:18 +0100

    optimised Roger approx working in 2 modes

--- feniax/aeromodal/roger.py
+++ feniax/aeromodal/roger.py
@@ -2,45 +2,51 @@ import jax.numpy as jnp
 import jax
 import pyNastran.op4.op4 as OP4
 import jax
+import itertools
+import plotly.express as px
+import plotly.graph_objects as go
+
 
 jax.config.update("jax_enable_x64", True)
 
 
-@jax.jit
-def frequency_matrixold(k_array, poles):
-    num_reducedfreq = len(k_array)
-    num_poles = len(poles)
-    k_array2 = k_array**2
-    even_ids = jnp.arange(0, num_reducedfreq * 2, 2)
-    odd_ids = jnp.arange(1, num_reducedfreq * 2, 2)
-    k_matrix = jnp.zeros((num_reducedfreq * 2, 2 + num_poles))
-    k_matrix = k_matrix.at[odd_ids, 0].set(k_array)
-    k_matrix = k_matrix.at[even_ids, 1].set(-k_array2)
-    for i, pi in enumerate(poles):
-        k_matrix = k_matrix.at[odd_ids, 2 + i].set((pi * k_array) / (k_array2 + pi**2))
-        k_matrix = k_matrix.at[even_ids, 2 + i].set(k_array2 / (k_array2 + pi**2))
 
-    return k_matrix
+class ComputeRoger:
 
+    def __init__(self, sampling_aeromatrices_, redfreqs_, poles_=None, option=1):
 
-@jax.jit
-def frequency_matrixold2(k_array, poles):
-    num_reducedfreq = len(k_array)
-    num_poles = len(poles)
-    k_array2 = k_array**2
-    # even_ids = jnp.arange(0, num_reducedfreq)#jnp.arange(0, num_reducedfreq * 2, 2)
-    # odd_ids = jnp.arange(num_reducedfreq, 2 * num_reducedfreq) #jnp.arange(1, num_reducedfreq * 2, 2)
-    # k_matrix = jnp.zeros((num_reducedfreq * 2, 2 + num_poles))
-    k_matrix = jnp.zeros((num_reducedfreq, 3 + num_poles), dtype=complex)
-    k_matrix = k_matrix.at[:, 0].set(k_array * 1j)
-    k_matrix = k_matrix.at[:, 1].set(-k_array2)
-    for i, pi in enumerate(poles):
-        k_matrix = k_matrix.at[:, 2 + i].set((k_array * 1j) / (k_array * 1j + pi))
-        # k_matrix = k_matrix.at[even_ids, 2 + i].set(
-        #  k_array2 / (k_array2 + pi ** 2))
-    kmatrix = jnp.vstack([k_matrix.real, k_matrix.imag])
-    return kmatrix
+        self.roger_matrices = None
+        self.sampling_aeromatrices = sampling_aeromatrices_
+        self.redfreqs = redfreqs_
+        if poles_ is not None:
+            self.poles = poles_
 
+    @property
+    def poles(self):
+
+        return self.poles
+
+    @poles.setter
+    def poles(self, _poles):
+
+        self.poles = _poles
+        self._solve()
+
+    def get_frequency_matrix(self):
+        ...
+    def _solve(self):
+        build_gafs(self.poles, self.sampling_aeromatrices, self.redfreqs)
+        build_gafs(self.poles, self.sampling_aeromatrices, self.redfreqs)
+
+        
+class EvaluateRoger:
+    ...
+
+class OptimisePoles:
+    ...
+
+class PlotGAFs:
+    ...
 
 @jax.jit
 def frequency_matrix(k_array, poles):
@@ -50,50 +56,45 @@ def frequency_matrix(k_array, poles):
     # even_ids = jnp.arange(0, num_reducedfreq)#jnp.arange(0, num_reducedfreq * 2, 2)
     # odd_ids = jnp.arange(num_reducedfreq, 2 * num_reducedfreq) #jnp.arange(1, num_reducedfreq * 2, 2)
     # k_matrix = jnp.zeros((num_reducedfreq * 2, 2 + num_poles))
+    k_matrix = jnp.zeros((num_reducedfreq, 2 + num_poles), dtype=complex)
+    #k_matrix = k_matrix.at[:, 0].set(1.0)
+    k_matrix = k_matrix.at[:, 0].set(k_array * 1j)
+    k_matrix = k_matrix.at[:, 1].set(-k_array2)
+    for i, pi in enumerate(poles):
+        k_matrix = k_matrix.at[:, 2 + i].set((k_array * 1j) / (k_array * 1j + pi))
+        # k_matrix = k_matrix.at[even_ids, 2 + i].set(
+        #  k_array2 / (k_array2 + pi ** 2))
+    kmatrix = jnp.vstack([k_matrix.real, k_matrix.imag])
+    return kmatrix
+
+@jax.jit
+def frequency_matrix2(k_array, poles):
+    num_reducedfreq = len(k_array)
+    num_poles = len(poles)
+    k_array2 = k_array**2
     k_matrix = jnp.zeros((num_reducedfreq, 3 + num_poles), dtype=complex)
     k_matrix = k_matrix.at[:, 0].set(1.0)
     k_matrix = k_matrix.at[:, 1].set(k_array * 1j)
     k_matrix = k_matrix.at[:, 2].set(-k_array2)
     for i, pi in enumerate(poles):
         k_matrix = k_matrix.at[:, 3 + i].set((k_array * 1j) / (k_array * 1j + pi))
-        # k_matrix = k_matrix.at[even_ids, 2 + i].set(
-        #  k_array2 / (k_array2 + pi ** 2))
+        
     kmatrix = jnp.vstack([k_matrix.real, k_matrix.imag])
     return kmatrix
 
 
 def stackQk_realimag(Qk):
-    num_freqs, Qrows, Qcols = Qk.shape
+    
     Qk_real = Qk.real
     Qk_imag = Qk.imag
-    # even_ids = jnp.arange(0, num_freqs * 2, 2)
-    # odd_ids = jnp.arange(1, num_freqs * 2, 2)
-    # Qk_new = jnp.zeros((num_freqs * 2, Qrows, Qcols))
-    # Qk_new = Qk_new.at[even_ids].set(Qk_real)
-    # Qk_new = Qk_new.at[odd_ids].set(Qk_imag)
     Qk_new = jnp.vstack([Qk_real, Qk_imag])
     return Qk_new
 
-
-def stackQk_realimagold(Qk):
-    num_freqs, Qrows, Qcols = Qk.shape
-    Qk_real = Qk.real
-    Qk_imag = Qk.imag
-    even_ids = jnp.arange(0, num_freqs * 2, 2)
-    odd_ids = jnp.arange(1, num_freqs * 2, 2)
-    Qk_new = jnp.zeros((num_freqs * 2, Qrows, Qcols))
-    Qk_new = Qk_new.at[even_ids].set(Qk_real)
-    Qk_new = Qk_new.at[odd_ids].set(Qk_imag)
-    return Qk_new
-
-
 @jax.jit
-def rogerRFAold(k_matrix, Qk):
-    num_freqs, Qrows, Qcols = Qk.shape
+def rogerRFA(k_matrix, Qk, A0):
+    
     k_matrix_inv = jnp.linalg.pinv(k_matrix)
-    A0 = Qk[0]
-    Qk1_ = Qk[2:]
-
+    
     def kernel(A0ij, Qij_k):
         Ap = k_matrix_inv @ (Qij_k - A0ij)
         return Ap
@@ -102,61 +103,17 @@ def rogerRFAold(k_matrix, Qk):
     loop_rows = jax.vmap(loop_cols, in_axes=(0, 1), out_axes=1)
 
     A0_reshaped = A0.reshape((1,) + A0.shape)
-    roger_matrices = loop_rows(A0, Qk1_)
+    roger_matrices = loop_rows(A0, Qk)
     return jnp.vstack([A0_reshaped, roger_matrices])
 
-
 @jax.jit
-def rogerRFAold2(k_matrix, Qk):
+def rogerRFA2(k_matrix, Qk, A0=None):
+    
     num_freqs, Qrows, Qcols = Qk.shape
-    k_matrix_inv = jnp.linalg.pinv(k_matrix)
-    A0 = Qk[0]
-    Qk1_ = Qk[
-        jnp.hstack(
-            [
-                jnp.arange(1, int(num_freqs / 2)),
-                jnp.arange(int(num_freqs / 2) + 1, num_freqs),
-            ]
-        )
-    ]
+    k_matrix_inv = jnp.linalg.pinv(k_matrix) # dim: states x numfreqs
 
-    def kernel(A0ij, Qij_k):
-        Ap = k_matrix_inv @ (Qij_k)
-        return Ap
-
-    loop_cols = jax.vmap(kernel, in_axes=(0, 1), out_axes=1)
-    loop_rows = jax.vmap(loop_cols, in_axes=(0, 1), out_axes=1)
-
-    A0_reshaped = A0.reshape((1,) + A0.shape)
-    roger_matrices = loop_rows(A0, Qk1_)
-    return jnp.vstack([A0_reshaped, roger_matrices])
-
-
-@jax.jit
-def rogerRFA(k_matrix, Qk):
-    num_freqs, Qrows, Qcols = Qk.shape
-    k_matrix_inv = jnp.linalg.pinv(k_matrix)
-    A0 = Qk[0]
-    Qk1_ = Qk[
-        jnp.hstack(
-            [
-                jnp.arange(1, int(num_freqs / 2)),
-                jnp.arange(int(num_freqs / 2) + 1, num_freqs),
-            ]
-        )
-    ]
-
-    def kernel(A0ij, Qij_k):
-        Ap = k_matrix_inv @ (Qij_k)
-        return Ap
-
-    loop_cols = jax.vmap(kernel, in_axes=(0, 1), out_axes=1)
-    loop_rows = jax.vmap(loop_cols, in_axes=(0, 1), out_axes=1)
-
-    A0_reshaped = A0.reshape((1,) + A0.shape)
-    roger_matrices = loop_rows(A0, Qk1_)
-    return roger_matrices  # jnp.vstack([A0_reshaped, roger_matrices])
-
+    roger_matrices = jnp.einsum('ij,jkl', k_matrix_inv, Qk)
+    return roger_matrices
 
 @jax.jit
 def Q_RFAki(ki, roger_matrices, poles):
@@ -168,25 +125,136 @@ def Q_RFAki(ki, roger_matrices, poles):
 
 Q_RFA = jax.vmap(Q_RFAki, in_axes=(0, None, None))
 
-if __name__ == "__main__":
+def err_ki(ki, Qki_dlm, roger_matrices, poles, order=None):
+    
+    Qki_roger = Q_RFAki(ki, roger_matrices, poles)
+    err = jnp.linalg.norm(Qki_dlm - Qki_roger, order) / jnp.linalg.norm(Qki_dlm, order)
+    return err
 
-    def err_ki(ki, Qki_dlm, roger_matrices, poles, order=None):
-        Qki_roger = Q_RFA(ki, roger_matrices, poles)
-        err = jnp.linalg.norm((Qki_dlm - Qki_roger), order)
-        return err
+err_k = jax.vmap(err_ki, in_axes=(0, 0, None, None, None))
 
-    err_k = jax.vmap(err_ki, in_axes=(0, 0, None, None, None))
+_err_dict = dict()
+def save(fun):
 
-    def compute_err(reduced_freqs, Qk_dlm, roger_matrices, poles, norm_order=None):
-        err = err_k(reduced_freqs, Qk_dlm, roger_matrices, poles, norm_order=None)
-        err_avg = err.sum() / len(err)
+    _err_dict[fun.__name__.split('_')[-1]] = fun
+    def wrap(*args, **kwargs):
+        
+        return fun(*args, **kwargs)
 
-        return err, err_avg
+    return wrap
+
+@save
+def err_average(reduced_freqs, Qk_dlm, roger_matrices, poles, norm_order=None):
+    
+    err = err_k(reduced_freqs, Qk_dlm, roger_matrices, poles, norm_order)
+
+    return jnp.average(err)
+
+@save
+def err_max(reduced_freqs, Qk_dlm, roger_matrices, poles, norm_order=None):
+    
+    err = err_k(reduced_freqs, Qk_dlm, roger_matrices, poles, norm_order)
+
+    return jnp.max(err)
+
+
+def read_sampling(op4_file: str, matrix_type="Q_HH") -> jnp.ndarray:
 
     op4 = OP4.OP4()
+    #op4model = op4.read_op4_ascii(op4_file)
+    op4model = op4.read_op4(op4_file)
+
+    try:
+        Qmatrices = jnp.array(op4model[matrix_type].data)
+    except AttributeError:
+        Qmatrices = jnp.array(op4model[matrix_type][1])
+        
+    return Qmatrices
+
+def build_gafs(poles, sampling_aeromatrices, redfreqs):
+
+    aeromatrices_stack = stackQk_realimag(sampling_aeromatrices[1:])
+    redfreqs_matrix = frequency_matrix(redfreqs[1:], poles)
+    A0 = sampling_aeromatrices[0] #jnp.vstack([sampling_aeromatrices[0], jnp.zeros_like(sampling_aeromatrices[0])])
+    roger_matrices = rogerRFA(redfreqs_matrix, aeromatrices_stack, A0)
+    return roger_matrices
+
+def build_gafs2(poles, sampling_aeromatrices, redfreqs):
+
+    aeromatrices_stack = stackQk_realimag(sampling_aeromatrices)
+    redfreqs_matrix = frequency_matrix2(redfreqs, poles)
+    roger_matrices2 = rogerRFA2(redfreqs_matrix, aeromatrices_stack)
+    return roger_matrices2
+
+vbuild_gafs = jax.vmap(build_gafs, in_axes=(0, None, None))
+vbuild_gafs2 = jax.vmap(build_gafs2, in_axes=(0, None, None))
+
+def err_poles(roger_matrices: jnp.ndarray,
+              poles: jnp.ndarray,
+              error_name: str,
+              redfreqs,
+              Qk_dlm,
+              norm_order):
+    
+    err_fun = _err_dict[error_name]
+    loss = err_fun(redfreqs, Qk_dlm, roger_matrices, poles, norm_order)
+    return loss
+
+verr_poles = jax.vmap(err_poles, in_axes=(0, 0, None, None, None, None))
+
+def build_polesgrid(num_poles, poles_step, poles_range):
+
+    poles = jnp.arange(poles_range[0],
+                       poles_range[1] + poles_step,
+                       poles_step)
+    poles_grid = jnp.array(list(itertools.combinations(poles, num_poles)))
+    return poles_grid
+    
+def optimise_brute(poles_grid, redfreqs, sampling_aeromatrices, error_name="average",
+                   norm_order=2
+                   ):
+
+    roger_matrices = vbuild_gafs2(poles_grid, sampling_aeromatrices, redfreqs)
+    verror = verr_poles(roger_matrices,
+               poles_grid,
+               error_name,
+               redfreqs,
+               sampling_aeromatrices,
+               norm_order)
+
+    min_index = jnp.argmin(verror)
+    return roger_matrices[min_index], poles_grid[min_index], verror[min_index]
+
+def plot_gafs(irow, jcolumn, Qdlm, Qroger):
+
+    fig = go.Figure()
+    
+    fig.add_trace(
+        go.Scatter(
+            x=Qdlm[:, irow, jcolumn].real,
+            y=Qdlm[:, irow, jcolumn].imag,
+            mode="markers",
+            # name='lines'
+        ),
+    )
+    for i, Qi in enumerate(Qroger):
+        fig.add_trace(
+            go.Scatter(
+                x=Qi[:, irow, jcolumn].real,
+                y=Qi[:, irow, jcolumn].imag,
+                mode="lines",
+                name=f"Q{i}"
+            ),
+        )
+    fig.show()
+
+
+if __name__ == "__main__":
 
     from pathlib import Path
     home = Path.home()
+    op4 = OP4.OP4()
+
     aero = op4.read_op4_ascii(f"{home}/pCloudDrive/tmp/Qhh50-50.op4")
     aero2 = op4.read_op4(f"{home}/pCloudDrive/tmp/Qhj0_8-50.op4")
 
@@ -197,84 +265,14 @@ if __name__ == "__main__":
         qhh = jnp.array(aero["Q_HH"][1])
         qhj = jnp.array(aero2["Q_HJ"][1])
 
-    k_array = jnp.linspace(1e-3, 1, 50)  # jnp.linspace(0,1,50)
-    poles = jnp.linspace(
-        1e-3, 1, 40
-    )  # jnp.array([0.05, 0.1, 0.15, 0.2, 0.5, 0.7, 0.9, 1.2, 1.6, 1.8, 2.5])
-    qhj_new = stackQk_realimag(qhj)
-    qhh_new = stackQk_realimag(qhh)
-
-    k_matrix = frequency_matrix(k_array, poles)
-    k_matrixold = frequency_matrixold(k_array, poles)
-    roger_matricesQhj = rogerRFA(k_matrix, qhj_new)
-    roger_matricesQhh = rogerRFA(k_matrix, qhh_new)
-
-    ks = jnp.hstack([1e-6, k_array])
-    Qk_hj = Q_RFA(ks, roger_matricesQhj, poles)
-    Qk_hh = Q_RFA(ks, roger_matricesQhh, poles)
-
-    PLOT = True
-    if PLOT:
-        import plotly.express as px
-        import plotly.graph_objects as go
-
-        i = 1
-        j = 50
-        #i = 20
-        #j = 18
-
-        fig = go.Figure()
-        # fig.add_trace(go.Scatter(x=qhh[:,i,j].real, y=qhh[:,i,j].imag),
-        #                     mode='makers',
-        #                     # name='lines'
-        #               )
-        fig.add_trace(
-            go.Scatter(
-                x=qhh[:, i, j].real,
-                y=qhh[:, i, j].imag,
-                mode="markers",
-                # name='lines'
-            ),
-        )
-
-        fig.add_trace(
-            go.Scatter(
-                x=Qk_hh[:, i, j].real,
-                y=Qk_hh[:, i, j].imag,
-                mode="lines",
-                # name='lines'
-            ),
-        )
-        # fig.add_trace(px.scatter(x=qhh[:,i,j].real, y=qhh[:,i,j].imag))
-
-        # fig = px.scatter(x=qhh[:,i,j].real, y=qhh[:,i,j].imag)
-        fig.show()
-
-        # Q_RFA(ki, roger_matrices, poles)
-
-        fig = go.Figure()
-        # fig.add_trace(go.Scatter(x=qhh[:,i,j].real, y=qhh[:,i,j].imag),
-        #                     mode='makers',
-        #                     # name='lines'
-        #               )
-        fig.add_trace(
-            go.Scatter(
-                x=qhj[:, i, j].real,
-                y=qhj[:, i, j].imag,
-                mode="markers",
-                # name='lines'
-            ),
-        )
-
-        fig.add_trace(
-            go.Scatter(
-                x=Qk_hj[:, i, j].real,
-                y=Qk_hj[:, i, j].imag,
-                mode="lines",
-                # name='lines'
-            ),
-        )
-        # fig.add_trace(px.scatter(x=qhh[:,i,j].real, y=qhh[:,i,j].imag))
-
-        # fig = px.scatter(x=qhh[:,i,j].real, y=qhh[:,i,j].imag)
-        fig.show()
+    num_poles = 7
+    poles_range = [0.01, 1]
+    poles_step = 0.1
+    polesgrid = build_polesgrid(num_poles, poles_step, poles_range)
+    k_array = jnp.linspace(1e-3, 1, 50)
+    ks = jnp.hstack([0, k_array])
+    roger_matrices, poles, verror = optimise_brute(polesgrid, ks, qhh)
+    roger_matrices2, poles2, verror2 = optimise_brute(jnp.array((jnp.linspace(0.01,1,num_poles),)), ks, qhh)
+    Qroger = Q_RFA(ks, roger_matrices, poles)
+    Qroger2 = Q_RFA(ks, roger_matrices2, poles2)
+    plot_gafs(2, 1, qhh, [Qroger, Qroger2])
