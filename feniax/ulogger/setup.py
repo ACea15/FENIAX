0309c1a81f3f9e19f3fe79360fd070419d1606a5
Alvaro Cea <alvaro_cea@outlook.com>
2025-02-27 17:04:46 +0000

    update logger to not repeat outputs in interactive session

--- feniax/ulogger/setup.py
+++ feniax/ulogger/setup.py
@@ -3,26 +3,30 @@ import pathlib
 
 def set_logging(config):
 
+
     # integrate with warning module
     logging.captureWarnings(True)
     
     # Create a root logger
     root_logger = logging.getLogger()
-    root_logger.setLevel(getattr(logging, config.log.level.upper()))
-
-    # Create a console handler with a console-specific format
-    console_handler = logging.StreamHandler()
-    console_formatter = logging.Formatter("%(name)s | %(levelname)s | %(message)s")
-    console_handler.setFormatter(console_formatter)
+    # Prevent adding multiple handlers
+    if not root_logger.hasHandlers():
     
-    log_file = config.log.path / f'{config.log.file_name}.log'
-    file_handler = logging.FileHandler(log_file, mode=config.log.file_mode)
-    file_formatter = logging.Formatter("%(name)s (L%(lineno)s): %(asctime)s | %(levelname)s | P %(process)d | %(message)s")
-    file_handler.setFormatter(file_formatter)
+        root_logger.setLevel(getattr(logging, config.log.level.upper()))
 
-    # Add the handlers to the root logger
-    root_logger.addHandler(console_handler)
-    root_logger.addHandler(file_handler)
+        # Create a console handler with a console-specific format
+        console_handler = logging.StreamHandler()
+        console_formatter = logging.Formatter("%(name)s | %(levelname)s | %(message)s")
+        console_handler.setFormatter(console_formatter)
+
+        log_file = config.log.path / f'{config.log.file_name}.log'
+        file_handler = logging.FileHandler(log_file, mode=config.log.file_mode)
+        file_formatter = logging.Formatter("%(name)s (L%(lineno)s): %(asctime)s | %(levelname)s | P %(process)d | %(message)s")
+        file_handler.setFormatter(file_formatter)
+
+        # Add the handlers to the root logger
+        root_logger.addHandler(console_handler)
+        root_logger.addHandler(file_handler)
 
 def get_logger(name):
 
